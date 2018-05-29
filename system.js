63a0b0dd96babf8c449c47adbc3ac27ba496f0f0
Lautenschlager <tai.romani@hotmail.com>
2018-05-29 14:31:49 -0300

    Update system.js

--- system.js
+++ system.js
@@ -101,12 +101,12 @@ var OneItem = function(prefix, id) {
 }
 
 // Translation system
-const LangName = {
+var const LangName = {
 	"EN": "English",
 	"BR": "Português",
 	"ES": "Español"
 }
-const Translations = {
+var const Translations = {
 	"EN": {
 		"me_0": "My name is Tainã Romani Lautenschlager Donda, also known as Lautenschlager or Tai. I am 17 and I am currently studying Computer Science at Uninove, in Brazil. I also have an informatic technician degree obtained in the educational institution ETEC Jaraguá.",
 		"me_1": "More than 3 years of experience with written translations (English and Spanish) and 2 years of experience with game and software development.",
